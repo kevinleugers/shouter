004aed956d5dec9630c4bb36db0657915c699ff5
kevindigital84 <kevin.leugers@gmail.com>
2014-02-07 23:11:50 -0500

    finished lesson 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,4 @@
 class User < ActiveRecord::Base
+	include Concerns::Following
 	has_many :shouts
-	
-	has_many :followed_user_relationships, foreign_key: :follower_id, class_name: 'FollowingRelationship'
-	has_many :followed_users, through: :followe_user_relationships
-
-	has_many :follower_relationships, foreign_key: :followed_user_id, class_name: 'FollowingRelationship'
-	has_many :followers, through: :follower_relationships
 end
