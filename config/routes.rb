8440a7f8488d930ce8b51df1cae72ccb3f1cb53b
kevinml <kevinml@kevinml-VirtualBox.(none)>
2014-02-09 07:05:07 -0500

    finished lesson 4

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Shouter::Application.routes.draw do
 	root to: 'homes#show', via: :get
 	resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
+	resource :search, only: [:show]
   resources :users, only: [:index, :new, :create, :show] do
 	  post 'follow' => 'following_relationships#create'
 		delete 'follow' => 'following_relationships#destroy'
@@ -9,4 +10,5 @@ Shouter::Application.routes.draw do
 	resources :shouts, only: [:show]
 	resources :text_shouts, only: [:create]
 	resources :photo_shouts, only: [:create]
+	resources :hashtags, only: [:show]
 end
